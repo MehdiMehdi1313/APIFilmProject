4ce3b52402a78705d28387961584fbc4681d894b
Gregoire Camoin <camoin.gregoire@outlook.fr>
2022-09-08 08:42:14 +0100

    Test des requêtes API

--- application.py
+++ application.py
@@ -15,7 +15,7 @@ headers = {
     'X-RapidAPI-Key': "732836899fmsh744957c5047c065p117016jsn132cb5d97924",
     'X-RapidAPI-Host': "imdb8.p.rapidapi.com"
     }
-
+# Obtenir la liste des id des acteurs nés aujourd'hui :
 conn.request("GET", "/actors/list-born-today?month=7&day=27", headers=headers)
 
 res = conn.getresponse()
@@ -26,7 +26,9 @@ list_id = data.decode("utf-8")
 x = json.loads(list_id)
 
 dict_name = {}
-for id in x :
+
+# Obtenir nom prénom des acteurs à partir de leur id :
+for id in x[:2] :
 
     id = id.split('/')[-2]
     conn.request("GET", "/actors/get-bio?nconst=" + str(id), headers=headers)
@@ -39,5 +41,16 @@ for id in x :
 
 print(dict_name)
 
+id = x[3].split('/')[-2]
 
+print('id = ' +str(id) + '\n')
 
+# Obtenir award, détail d'un acteur, image à partir de son id (et afficher son nom, prénom)
+"""
+conn.request("GET", "/actors/get-awards-summary?nconst=" + str(id), headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
+"""
