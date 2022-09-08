7e42d3b77cd12f19dfe4c653d4526127e1bc75e9
Gregoire Camoin <camoin.gregoire@outlook.fr>
2022-09-08 09:54:01 +0100

    Ajout des requête permettant de savoir les award gagné par l'acteur et d'avoir sa photo

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ def movie():
 conn = http.client.HTTPSConnection("imdb8.p.rapidapi.com")
 
 headers = {
-    'X-RapidAPI-Key': "732836899fmsh744957c5047c065p117016jsn132cb5d97924",
+    'X-RapidAPI-Key': "2291b34504msh32e737e169ea76ap1def00jsn4ee3554760e2",
     'X-RapidAPI-Host': "imdb8.p.rapidapi.com"
     }
 # Obtenir la liste des id des acteurs nés aujourd'hui :
@@ -28,7 +28,7 @@ x = json.loads(list_id)
 dict_name = {}
 
 # Obtenir nom prénom des acteurs à partir de leur id :
-for id in x[:2] :
+for id in x[:1] :
 
     id = id.split('/')[-2]
     conn.request("GET", "/actors/get-bio?nconst=" + str(id), headers=headers)
@@ -46,11 +46,36 @@ id = x[3].split('/')[-2]
 print('id = ' +str(id) + '\n')
 
 # Obtenir award, détail d'un acteur, image à partir de son id (et afficher son nom, prénom)
-"""
+
 conn.request("GET", "/actors/get-awards-summary?nconst=" + str(id), headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
-"""
+#Récupérer les award
+
+awards = data.decode("utf-8")
+
+json_awards= json.loads(awards)
+#name = json_bio['name']
+try:
+    print(json_awards['awardsSummary']['highlighted']['awardName'])
+    award = json_awards['awardsSummary']['highlighted']['awardName']
+    
+except KeyError:
+    print("Pas d'award")
+    award = "Pas d'award"
+
+print('\n')
+
+# Photo acteur :
+conn.request("GET", "/actors/get-bio?nconst=" + str(id), headers=headers)
+res = conn.getresponse()
+
+data = res.read()
+detail = data.decode("utf-8")
+json_detail= json.loads(awards)
+url_image = json_detail['image']['url']
+
+print('\n')
+print(url_image)
