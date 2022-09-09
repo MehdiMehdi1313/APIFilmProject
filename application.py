010df07bb1ee0d6b9ea9732b4774189d4859444b
Gregoire Camoin <camoin.gregoire@outlook.fr>
2022-09-09 09:17:46 +0100

    Ajout des fonctions élémentaires

--- application.py
+++ application.py
@@ -23,44 +23,77 @@ headers = {
     'X-RapidAPI-Key': "2291b34504msh32e737e169ea76ap1def00jsn4ee3554760e2",
     'X-RapidAPI-Host': "imdb8.p.rapidapi.com"
     }
+
 # Obtenir la liste des id des acteurs nés aujourd'hui :
+def actors(strReq) :
 
-x = requete("/actors/list-born-today?month=7&day=27")
+    x = requete(strReq)
 
-dict_name = {}
+    dict_name = {}
 
-# Obtenir nom prénom des acteurs à partir de leur id et les placer dans un dict :
-for id in x[:1] :
+    # Obtenir nom prénom des acteurs à partir de leur id et les placer dans un dict :
+    for id in x[:1] :
 
-    id = id.split('/')[-2]
+        id = id.split('/')[-2]
 
-    json_bio = requete("/actors/get-bio?nconst=" + str(id))
-    name = json_bio['name']
-    dict_name[id] = name
+        json_bio = requete("/actors/get-bio?nconst=" + str(id))
+        name = json_bio['name']
+        dict_name[id] = name
 
-print(dict_name)
+    #print(dict_name)
 
-id = x[3].split('/')[-2] # Pour ne garder que la partie où on a l'id.
+    id = x[3].split('/')[-2] # Pour ne garder que la partie où on a l'id.
 
-print('id = ' +str(id) + '\n')
+    #print('id = ' +str(id) + '\n')
+
+    return dict_name, id
 
 # Obtenir award, détail d'un acteur, image à partir de son id (et afficher son nom, prénom)
-json_awards = requete("/actors/get-awards-summary?nconst=" + str(id))
 
-#name = json_bio['name']
-try:
-    print(json_awards['awardsSummary']['highlighted']['awardName'])
-    award = json_awards['awardsSummary']['highlighted']['awardName']
-    
-except KeyError:
-    print("Pas d'award")
-    award = "Pas d'award"
+def awards(strReq) :
 
-print('\n')
+    json_awards = requete(strReq)
+
+    #name = json_bio['name']
+    try:
+        award = json_awards['awardsSummary']['highlighted']['awardName']
+        
+    except KeyError:
+        award = "Pas d'award"
+
+    print('\n')
+    return award
 
 # Photo acteur :
-json_detail = requete("/actors/get-bio?nconst=" + str(id))
-url_image = json_detail['image']['url']
 
-print('\n')
-print(url_image)
+def photo(strReq) : 
+    json_detail = requete(strReq)
+    url_image = json_detail['image']['url']
+    return url_image
+
+# Films les plus populaires :
+
+def title(strReq) :
+
+    json_title = requete(strReq)
+    json_title = json_title[:5]
+    json_title = list(map(lambda x : x.split('/')[-2] , json_title))
+    return json_title
+
+
+dict_name, id = actors("/actors/list-born-today?month=7&day=27")
+
+award = awards("/actors/get-awards-summary?nconst=" + str(id))
+
+url_image = photo("/actors/get-bio?nconst=" + str(id))
+
+json_title = title("/title/get-most-popular-movies?homeCountry=US&purchaseCountry=US&currentCountry=US")
+
+print(id + '\n')
+
+print(award + '\n')
+
+print(url_image + '\n')
+
+print(json_title)
+
